f171a16fa78c9e0c9dedc9053e6d7caac5f10af4
Warren Bain <warren@ninefold.com>
2014-03-20 16:21:43 +1100

    Update format to expose exceptions and debug info

--- lib/chef/handler/ninefold_communicator.rb
+++ lib/chef/handler/ninefold_communicator.rb
@@ -28,14 +28,41 @@ module Ninefold
       end
 
       def report
-        if run_status.failed? && !ignore_exception(run_status.exception)
-          Chef::Log.error "#{tag} Reporting exception via Ninefold Communicator"
+        if run_failed? && !ignore_exception(run_exception)
+          # we report the formatted exception with a checkpoint so that
+          # Portal can extract it and report to customer via UI / CLI
+          msg = tag
+          msg << " We detected that chef run #{run_id} failed for the following reason:\n"
+          msg << " #{formatted_exception}\n"
+          msg << " Please contact Ninefold Support if you require further assistance\n"
+          Chef::Log.fatal msg
         else
-          Chef::Log.info "#{tag} Reporting success via Ninefold Communicator"
+          Chef::Log.info "#{tag} chef #{run_id} succeeded!"
         end
+        Chef::Log.debug run_information
       end
 
       protected
+
+      def run_failed?
+        run_status.failed?
+      end
+
+      def run_id
+        run_status.run_id
+      end
+
+      def run_exception
+        run_status.exception
+      end
+
+      def formatted_exception
+        run_status.formatted_exception
+      end
+
+      def run_information
+        run_status.to_hash
+      end
 
       def ignore_exception?(exception)
         ignore.any? do |ignore_case|
