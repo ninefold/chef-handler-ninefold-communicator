046945d5266313bf1a3d3ece23a3f277c5f58a53
Warren Bain <warren@ninefold.com>
2014-03-25 17:57:31 +1100

    Update to add proper formatted exception

--- lib/chef/handler/ninefold_communicator.rb
+++ lib/chef/handler/ninefold_communicator.rb
@@ -55,7 +55,7 @@ module Ninefold
       end
 
       def marker_copy
-        "#{marker}_START #{run_exception} #{marker}_END"
+        "#{marker}_START #{formatted_exception} #{marker}_END"
       end
 
       def prettify(*lines)
